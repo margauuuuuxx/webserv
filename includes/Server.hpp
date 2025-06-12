c19c9f93e096ee606c7e596390a69d8f4fbd3c49
margauuuuuxx <marlonco@shi-c1-p3.s19.be>
2025-06-12 15:05:16 +0200

    opti progress

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -9,14 +9,19 @@
         - holding configuration and client list
 */
 
+static const int    BACKLOG = 10;
+static const int    BUFFER_SIZE = 10000;
+
 class Server
 {
     private:
         Server(const Server &other);
         Server& operator=(const Server &other);
+        
+        void        run();
 
         int         _sockfd;
-        int         _port;
+        uint16_t    _port;
         sockaddr_in _address;
         socklen_t   _addrlen;
 
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -1,52 +1,67 @@
 #include "../includes/Server.hpp"
 
-Server::Server() 
+Server::Server() : _port(8080), _addrlen(sizeof(_address))
 {
     _sockfd = socket(PF_INET, SOCK_STREAM, 0);
     if (_sockfd < 0)
         errorExit("Failed to create socket");
 
-    _port = 8080;
-    memset(&_address, 0, sizeof(_address));
-    _address.sin_family = AF_INET;
-    _address.sin_port = htons(_port);
-    _address.sin_addr.s_addr = htonl(INADDR_ANY);
-    _addrlen = sizeof(_address);
-    
     int reuse = 1;
     if (setsockopt(_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
         errorExit("Function setsockopt failed");
 
+    memset(&_address, 0, sizeof(_address));
+    _address.sin_family = AF_INET;
+    _address.sin_port = htons(_port);
+    _address.sin_addr.s_addr = htonl(INADDR_ANY);
+    
     if (bind(_sockfd, (const struct sockaddr *) &_address, sizeof(_address)) < 0)
         errorExit("Failed to bind");
 
-    if (listen(_sockfd, 10) < 0) // 10 is arbitrary # --> NEEDS TO CHANGE
-        errorExit("Failed to listen for sockfd = " + _sockfd);
+    if (listen(_sockfd, BACKLOG) < 0)
+        errorExit("Listen function failed");
 
+    run();
+}
+
+Server::~Server() {}
+
+void    Server::run()
+{
+    char buffer[BUFFER_SIZE];
+    
     while (1)
     {
-        char buffer[10000] = {0};
+        memset(buffer, 0, BUFFER_SIZE);
 
         int newSocket = accept(_sockfd, (struct sockaddr *) &_address, &_addrlen);
         if (newSocket < 0)
-            errorExit("Accept function failed for socket fd = " + newSocket);
+            errorExit("Accept function failed");
     
-        int bytesRead = read(newSocket, buffer, 10000);
+        int bytesRead = read(newSocket, buffer, BUFFER_SIZE);
         if (bytesRead < 0)
+        {
+            close(newSocket);
             errorExit("No bytes to read");
+        }
         if (bytesRead == 0)
+        {
+            close(newSocket);
             errorExit("No bytes read: Client disconnected");
+        }
     
         std::string pageContent = loadFile("index.html");
         std::cout << "Page content size = " << pageContent.length() << std::endl;
         std::string header = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: " + intToString(pageContent) + "\r\n\r\n";
         std::string response = header + pageContent;
 
-        std::cout << header << std::endl;
+        ssize_t sent = write(newSocket, response.c_str(), response.length());
+        if (sent < 0)
+        {
+            close(newSocket);
+            errorExit("Failed to write to client socket");
+        }
 
-        write(newSocket, response.c_str(), response.length());
         close(newSocket);
     }
 }
-
-Server::~Server() {}
