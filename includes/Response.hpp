7a1a078a87d0480ffdd3908864646114afd39969
margauuuuuxx <margaux.loncour@gmail.com>
2025-08-14 16:49:43 +0200

    work on methods

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -2,21 +2,27 @@
 
 #include "includes.hpp"
 
+// Forward-declare classes to reduce header dependencies
+class Request;
+class Server;
+
 class Response {
 public:
-	Response();
-	~Response();
+    Response();
+    ~Response();
 
-	void		handleRequest(Request& req, Server& server);
-	static void	handleGET(Request& req, Server& server);
-	static void	handlePOST(Request& req, Server& server);
-	static void	handleDELETE(Request& req, Server& server);
-	static void	buildErrorResponse(int code, Request& req, Server& server);
-	std::string	getResponse();
+    void        handleRequest(Request& req, Server& server);
+    std::string getResponse();
 
 private:
-	std::string	_content;
-	std::string	_httpVersion;
-	size_t		_contentSize;
-	size_t		_statusCode;
+    void handleGET(Request& req, Server& server);
+    void handlePOST(Request& req, Server& server);
+    void handleDELETE(Request& req, Server& server);
+    void buildErrorResponse(int code, Request& req, Server& server);
+    Route*  findRoute(const std::string& location, Server &server);
+
+    std::string _content;
+    std::string _httpVersion;
+    size_t      _contentSize;
+    size_t      _statusCode;
 };
--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -1,176 +1,188 @@
 #include "../includes/includes.hpp"
 
-/*
-	This class is responsible for building the appropriate HTPP response for a given request.
-*/
-
-Response::Response() {}
+Response::Response() : _contentSize(0), _statusCode(0) {}
 
 Response::~Response() {}
 
-static void	Response::buildErrorResponse(int code, Request& req, Server& server)
+// Efficiently reads an entire file into a string.
+static bool readFile(const std::string& path, std::string& content) {
+    std::ifstream file(path.c_str(), std::ios::in | std::ios::binary);
+    if (!file) {
+        return false;
+    }
+    // Seek to the end of the file to determine its size.
+    file.seekg(0, std::ios::end);
+    content.resize(file.tellg());
+    // Seek back to the beginning and read the whole file.
+    file.seekg(0, std::ios::beg);
+    file.read(&content[0], content.size());
+    file.close();
+    return true;
+}
+
+Route*  Response::findRoute(const std::string location, Server &server)
 {
-	this->_statusCode = code;
-	this->_httpVersion = req.getVersion();
+    for (std::vector<Route>::const_iterator it = server.routes.begin(); it != server.routes.end(); ++it) {
+        if (it.location == location) 
+            return (route = &(*it));
+    }
 
-	if (server.errorPages(code)) { // CHECK THIS LINE NOT SURE HOW IT WORKS 
-		std::string errorPagePath = server.errorPage[code]; // CHECK HOW THE PATH WORKS (I.E. ROOTING PATH)
-		std::ifstream file(errorPagePath.c_str());
-		if (file.is_open())
-		{
-			std::string line;
-			while (std::getline(file, line)) {
-				this->_content += line; // QUID CONTENT SIZE ??
-			}
-			file.close();
-		}
-	}
-
-	if (this->_content.empty())
-	{
-		std::string errorMessage = "An error occured ...";
-		if (this->_statusCode == 405)
-			errorMessage = "Method not allowed ...";
-	
-	}
+    return (NULL);    
 }
 
-static void	Response::handleGET(Request& req, Server& server) {
-	std::string location = req.getContent();
-	Route* route = NULL;
-	std::cout << "location: " << location << std::endl;
-	for(size_t i = 0; i < server.routes.size(); i++){
-		if (server.routes[i].location == location){
-			route = &server.routes[i];
-			break;
-		}
-	}
-	if (!route) {
-		std::cout << "bad location" << std::endl;
-		this->_statusCode = 404;
-		this->_httpVersion = req.getVersion();
-		this->_contentSize = 0;
-		return ;	
-	}
-	std::cout << "route: " << route->location << std::endl;
+void Response::buildErrorResponse(int code, Request& req, Server& server) {
+    this->_statusCode = code;
+    this->_httpVersion = req.getVersion();
 
-	std::ifstream file;
-	std::string filename;
-	bool found = 0;
-	for (size_t i = 0; i < route->index.size(); i++) {
-		 filename = route->root + "/" + route->index[i]; 
-		std::cout << "filename: " << filename << std::endl;
-		file.open(filename.c_str());
-		if (file.is_open()){
-			std::cout << "good filename: " << filename << std::endl;
-			found = 1;
-			break;
-		}
-	}
-	//need protection in case of bad filename 
-	if (!found){
-		std::cout << "NOT found" << std::endl;
-		this->_statusCode = 404;
-		this->_httpVersion = req.getVersion();
-		this->_contentSize = 0;
-		return ;
-	}
-	std::string line;
-	while (std::getline(file, line)){
-		this->_content += line;
-	}
-	file.close();
-	std::cout << "content: " << std::endl;
-	std::cout << this->_content << std::endl;
-	this->_contentSize = this->_content.size();
-	this->_statusCode = 200;
-	this->_httpVersion = req.getVersion();
-	
+    // Check for a custom error page in the server config.
+    std::map<int, std::string>::const_iterator it = server.errorPages.find(code);
+    if (it != server.errorPages.end()) { // CHECK FOR ERROR PAGES 
+        if (readFile(it->second, this->_content)) {
+            this->_contentSize = this->_content.size();
+            return;
+        }
+    }
+
+    // If no custom page, create a default one.
+    std::ostringstream oss;
+    oss << "<html>\n<head><title>" << this->_statusCode << " " << statusMessages[this->_statusCode]
+        << "</title></head>\n<body>\n<h1>" << this->_statusCode << " " << statusMessages[this->_statusCode]
+        << "</h1>\n</body>\n</html>";
+    this->_content = oss.str();
+    this->_contentSize = this->_content.size();
 }
 
-/*
-	POST:
-		1. reads the date from the request body
-		2. identify what to do with it 
-		3. send back a response with status code 
-*/
-static void	Response::handlePOST(Request& req, Server& server)
+bool    isCGIReq(const std::string& resource, const Route* route)
 {
+    if (route.cgiPath.empty())
+        return (false);
 
+    size_t dotPos = resource.rfind('.');
+    if (dotPos == std::string::npos)
+        return (false);
+
+    std::string ext = resource.substr(dotPos);
+    for (size_t i = 0; i < route.cgiExtension.size(); ++i) {
+        if (ext == route.cgiExtension[i])
+            return (true);
+    }
+
+    return (false);
 }
 
-/*
-		DELETE:
-			1. identify the resource to be deleted
-			2. attempts to delete it
-			2. sends back a response with status code
-*/
-static void	Response::handleDELETE(Request& req, Server& server)
+void    handleRoute(Request& req, Server& server, Route* route)
 {
+    std::string resource = req.getContent();
+
+    if (isCGIReq(resource, route))
+        handleCGI(req, server, route);
+
+    else if (route->uploadEnabled)
+        handleUpload(req, server, route);
+    else
+        handleStaticFile(req, server, route);
+}
+
+void Response::handleGET(Request& req, Server& server) {
+    Route* route = findRoute(req.getContent(), server);
+    
+    if (!route) {
+        std::cout << "bad location" << std::endl;
+        return buildErrorResponse(404, req, server);
+    }
+
+    std::cout << "route: " << route->location << std::endl;
+
+    for (size_t i = 0; i < route->index.size(); i++) {
+        std::string filename = route->root + "/" + route->index[i];
+        std::cout << "filename: " << filename << std::endl;
+        if (readFile(filename, this->_content)) {
+            std::cout << "good filename: " << filename << std::endl;
+            this->_statusCode = 200;
+            this->_httpVersion = req.getVersion();
+            this->_contentSize = this->_content.size();
+            return; // Success
+        }
+    }
+
+    // If no index file was found or readable
+    std::cout << "NOT found" << std::endl;
+    return buildErrorResponse(404, req, server);
+}
+
+void Response::handlePOST(Request& req, Server& server) {
+    Route* route = findRoute(req.getContent(), server);
+
+    if (!route)
+    {
+        buildErrorResponse(404, req, server);
+        return;
+    }
+
+    if (req.getBody().size() > server.clientMaxBodySize)
+    {
+        buildErrorResponse(413, req, server); // 413 Playload Too Large
+        return;
+    }
+
+    
 
 }
 
-typedef void	(*HandlerFct)(const Request&, Server&);
-void	Response::handleRequest(Request& req, Server& server)
-{
-	std::string method = req.getMethod();	
-	this->_httpVersion = req.getVersion();
-
-	std::cout << "===RESPONSE SETUP===" << std::endl;
-
-	std::map<std::string, HandlerFct> handlers;
-	handlers["GET"] = &handleGET;
-	handlers["POST"] = &handlePOST;
-	handlers["DELETE"] = &handleDELETE;
-
-	std::map<std::string, HandlerFct>::iterator it = handlers.find(method);
-	if (it != handlers.end())
-		it->second(req, server);
-	else
-		this->_statusCode = 405;
+void Response::handleDELETE(Request& req, Server& server) {
+    // Not implemented yet. Responding with an error.
+    (void)req; // Suppress unused parameter warning
+    (void)server; // Suppress unused parameter warning
+    buildErrorResponse(501, req, server); // 501 Not Implemented
 }
 
-std::string	Response::getResponse() {
-    // Table de correspondance code → message
+typedef void (Response::*HandlerFct)(Request&, Server&);
+void Response::handleRequest(Request& req, Server& server) {
+    this->_httpVersion = req.getVersion();
+
+    std::cout << "===RESPONSE SETUP===" << std::endl;
+
+    std::map<std::string, HandlerFct> handlers;
+    handlers["GET"] = &Response::handleGET;
+    handlers["POST"] = &Response::handlePOST;
+    handlers["DELETE"] = &Response::handleDELETE;
+
+    std::string method = req.getMethod();
+    if (handlers.count(method)) {
+        (this->*(handlers[method]))(req, server);
+
+    std::map<std::string, HandlerFct>::const_iterator it = handlers.find(req.getMethod());
+    if (it != handlers.end())
+        (this->*(it->second))(req, server);
+    else
+        buildErrorResponse(405, req, server);
+}
+
+std::string Response::getResponse() {
     static std::map<size_t, std::string> statusMessages;
     if (statusMessages.empty()) {
         statusMessages[200] = "OK";
         statusMessages[201] = "Created";
         statusMessages[204] = "No Content";
-        statusMessages[301] = "Moved Permanently";
-        statusMessages[302] = "Found";
-        statusMessages[400] = "Bad Request";
-        statusMessages[403] = "Forbidden";
         statusMessages[404] = "Not Found";
         statusMessages[405] = "Method Not Allowed";
-        statusMessages[500] = "Internal Server Error";
         statusMessages[501] = "Not Implemented";
-        statusMessages[502] = "Bad Gateway";
-        statusMessages[503] = "Service Unavailable";
     }
 
     std::ostringstream res;
-    std::ostringstream num;
-
-    // Ligne de statut
-    res << this->_httpVersion << " ";
-    num << this->_statusCode;
-    res << num.str() << " ";
-    if (statusMessages.find(this->_statusCode) != statusMessages.end())
+    res << this->_httpVersion << " " << this->_statusCode << " ";
+    if (statusMessages.count(this->_statusCode)) {
         res << statusMessages[this->_statusCode];
-    else
-        res << "Unknown";
+    } else {
+        res << "Unknown Status";
+    }
     res << "\r\n";
 
-    // En-têtes
-    std::ostringstream len;
-    len << this->_contentSize;
-    res << "Content-Length: " << len.str() << "\r\n";
+    res << "Content-Length: " << this->_contentSize << "\r\n";
     res << "Content-Type: text/html\r\n";
     res << "Connection: close\r\n";
-    res << "\r\n"; // ligne vide
+    res << "\r\n";
 
-    // Corps
     res << this->_content;
 
     return res.str();
