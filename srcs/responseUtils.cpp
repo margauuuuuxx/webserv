ee494f1e89ea6c08084e08a895d1ad8cad9e79a8
margauuuuuxx <margaux.loncour@gmail.com>
2025-09-26 11:14:34 +0200

    custom error pages fix

--- srcs/responseUtils.cpp
+++ srcs/responseUtils.cpp
@@ -42,8 +42,7 @@ void    Response::_buildResponse(int statusCode, Request& req, bool upload, cons
 void Response::buildErrorResponse(int code, Request& req, Server& server) {
     this->_statusCode = code;
     this->_httpVersion = req.getVersion();
-    _setHeaders("text/html");
-
+    
     // Check for a custom error page in the server config.
     std::map<int, std::string>::const_iterator it = server.errorPages.find(code);
     if (it != server.errorPages.end()) { // CHECK FOR ERROR PAGES 
@@ -53,19 +52,21 @@ void Response::buildErrorResponse(int code, Request& req, Server& server) {
             return;
         }
     }
-
+    
     // If no custom page, create a default one.
     std::ostringstream oss;
     std::string statusMessage = "Unknown status";
     std::map<int, std::string>::const_iterator it2 = _statusMessages.find(this->_statusCode);
     if (it2 != _statusMessages.end())
-        statusMessage = it2->second;
-    
+    statusMessage = it2->second;
+
     oss << "<html>\n<head><title>" << this->_statusCode << " " << statusMessage
-        << "</title></head>\n<body>\n<h1>" << this->_statusCode << " " << statusMessage
-        << "</h1>\n</body>\n</html>";
+    << "</title></head>\n<body>\n<h1>" << this->_statusCode << " " << statusMessage
+    << "</h1>\n</body>\n</html>";
+    
     this->_content = oss.str();
     this->_contentSize = this->_content.size();
+    _setHeaders("text/html");
 }
 
 void    Response::buildCGIResponse(CGI& CGIobj) {
